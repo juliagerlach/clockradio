93601edde0eaf8c1e10c7c4d735d5387fbc0ebea
Julia Gerlach <juliagerlach442@gmail.com>
2018-05-31 15:38:19 -0500

    updating program files

---
+++ repos/ClockRadio/ClockRadio/ClockRadio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockradioProject
+{
+    public class ClockRadio
+    {
+        //member variables
+        public string timeOfDay;
+        public string currentStation;
+        public bool alarmStatus;
+        public string timeOfAlarm;
+        public string newStation;
+        public string newAlarmTime;
+
+        //constructor
+        public ClockRadio(string timeOfDay, string currentStation, bool alarmStatus)
+        {
+            this.timeOfDay = "5:00 pm";
+            this.currentStation = "88.9 fm";
+            this.alarmStatus = true;
+            this.timeOfAlarm = "12:00am";
+        }
+
+
+        //member methods
+        public void ChecksStatus()
+        {
+            Console.WriteLine("\nThe current time is 5:00 pm.");
+            Console.WriteLine("\nThe alarm is set for 12:00 am.");
+            Console.WriteLine("\nThe radio is currently tuned to 88.9 fm.");
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+            Console.ReadKey(true);
+        }
+
+        public void SetsAlarm()
+        {
+            Console.WriteLine("\nWhat time would you like the alarm to sound?");
+            newAlarmTime = Console.ReadLine();
+            Console.WriteLine($"\nThe alarm has been changed to sound at {newAlarmTime}.");
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadKey(true);
+        }
+        public bool ChangesStation()
+        {
+            Console.WriteLine("\nEnter the call numbers of the station you would like to switch to");
+            newStation = Console.ReadLine();
+            Console.WriteLine($"\nThe station will be changed to {newStation}.");
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadKey(true);
+            return true;
+        }
+    }
+}
--- repos/ClockRadio/ClockRadio/Program.cs
+++ repos/ClockRadio/ClockRadio/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClockRadio
+namespace ClockradioProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ClockRadio myClockRadio = new ClockRadio("5:00pm", "88.9", true);
+
+            myClockRadio.ChecksStatus();
+
+            myClockRadio.SetsAlarm();
+            Console.WriteLine(myClockRadio.newAlarmTime);
+
+            myClockRadio.ChangesStation();
+            Console.WriteLine(myClockRadio.newStation);
         }
     }
 }
